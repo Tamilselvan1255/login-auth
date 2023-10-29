2fbdee40353c5601161483569d8639a8eacb3fca
Tamilselvan1255 <tamilselvan0677@gmail.com>
2023-10-29 21:28:03 +0530

    changed

--- src/Register.js
+++ src/Register.js
@@ -49,7 +49,7 @@ const Register = () => {
         e.preventDefault();
         let regobj = { id, name, password, email, phone, country, address, gender };
         if(IsValidate()){
-        fetch("https://login-auth-rho.vercel.app/user", {
+        fetch("http://localhost:8000/user/", {
             method: "POST",
             headers: { 'content-type': 'application/json' },
             body: JSON.stringify(regobj)
